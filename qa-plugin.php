1a22aa05901643620c7917fb2d1372005d997cf1
Arjun Suresh <arjunsuresh1987@gmail.com>
2016-07-07 18:49:12 +0000

    metadata removed from qa-plugin

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,17 +1,5 @@
 <?php
 
-/*
-        Plugin Name: TagSearch
-        Plugin URI: https://github.com/arjunsuresh/chat
-        Plugin Update Check URI: https://raw.github.com/arjunsuresh/chat/master/qa-plugin.php
-        Plugin Description: Adds option to search questions by tag(s) to sidepanel
-        Plugin Version: 1.1
-        Plugin Date: 2016-07-05
-        Plugin Author: Arjun
-        Plugin Author URI: http://gateoverflow.in 
-        Plugin License: GPLv2
-        Plugin Minimum Question2Answer Version: 1.7
-*/
 
 
 	if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
